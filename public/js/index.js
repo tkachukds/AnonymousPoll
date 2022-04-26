fd09b5b6a598dc92c0bab235623d6799e0bc2ceb
Denis <103854079+tkachukds@users.noreply.github.com>
2022-04-27 01:18:46 +0300

    create anim and limit correct

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,12 @@
 jQuery.noConflict()(function ($) { //чтобы не было ошибок из-за доллара
 	$(document).ready(function(){//dom прочитан
 
-let new_number_textarea=0 //сколько колличество раз можно создать вариант ответа
+let new_number_textarea=0 
+let limit_new_textarea = 20//сколько колличество раз можно создать вариант ответа
 
 function create_textarea() {  //создать вариант ответа-новую textarea
 	$(document).ready(function(){
-		if (new_number_textarea<4){
+		if (new_number_textarea<limit_new_textarea){
 			$("<textarea name='question' maxlength='512' style='color:red;' placeholder='Вариант ответа' rows='1' cols='30px' class='q-text'></textarea><br>").insertBefore(".uk-form-controls");	
 			new_number_textarea++;
 			console.log(new_number_textarea);
