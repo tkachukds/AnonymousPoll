d4069c0d7af6deee61e8c08aa04690c0c337796e
Denis <103854079+tkachukds@users.noreply.github.com>
2022-04-27 01:05:39 +0300

    create anim and limit v1

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,27 @@
+jQuery.noConflict()(function ($) { //чтобы не было ошибок из-за доллара
+	$(document).ready(function(){//dom прочитан
 
-function create_textarea() {
+let new_number_textarea=4 //сколько колличество раз можно создать вариант ответа
+
+function create_textarea() {  //создать вариант ответа-новую textarea
 	$(document).ready(function(){
+		
 			$("<textarea name='question' maxlength='512' style='color:red;' placeholder='Вариант ответа' rows='1' cols='30px' class='q-text'></textarea><br>").insertBefore(".uk-form-controls");	
-	});
-}
+			new_number_textarea--;
+		});
+	}
+//анимируем начало			               }
+setTimeout(function() { create_textarea(); }, 0500)
+setTimeout(function() { create_textarea(); }, 1000)
 
-jQuery.noConflict()(function ($) {
-
-	$(".AddComment").click(function () {
+//кнопка добавить вариант овтета
+$(".AddComment").click(function () {
+if (new_number_textarea>=1)
 create_textarea();
 });
-})
 
-setTimeout(function() { create_textarea(); }, 1000)
-setTimeout(function() { create_textarea(); }, 2000)
+
+//конец, печатать
+})
+});
+
